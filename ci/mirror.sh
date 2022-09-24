178ec2e09f7f0d9031a8bd42aa216f81f8aec247
Idriss Neumann <idriss.neumann@comwork.io>
2022-09-24 10:33:56 +0100

    Fix: remove bitbucket

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -6,6 +6,5 @@ cd "${REPO_PATH}" && git pull origin main || :
 git reset --hard origin/main
 git push -f github main
 git push -f pgitlab main
-git push -f bitbucket main
 git push -f froggit main
 exit 0
